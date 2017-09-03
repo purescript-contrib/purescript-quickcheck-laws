a80fec40530c90b70596d082f7643f9bb3066adf
Jorge Acereda <jacereda@gmail.com>
2017-09-03 18:47:01 +0200

    Warnings

--- src/Test/QuickCheck/Laws.purs
+++ src/Test/QuickCheck/Laws.purs
@@ -4,8 +4,9 @@ module Test.QuickCheck.Laws
   ) where
 
 import Prelude
+
 import Control.Monad.Eff.Console (log)
-import Data.Enum (class BoundedEnum)
+import Data.Enum (class BoundedEnum, class Enum)
 import Data.Monoid (class Monoid)
 import Test.QuickCheck (QC)
 import Test.QuickCheck.Arbitrary (class Arbitrary, class Coarbitrary)
@@ -21,6 +22,7 @@ derive newtype instance arbitraryA ∷ Arbitrary A
 derive newtype instance boundedA ∷ Bounded A
 derive newtype instance boundedEnumA :: BoundedEnum A
 derive newtype instance coarbitraryA ∷ Coarbitrary A
+derive newtype instance enumA :: Enum A
 derive newtype instance eqA ∷ Eq A
 derive newtype instance ordA ∷ Ord A
 derive newtype instance semigroupA ∷ Semigroup A
@@ -32,6 +34,7 @@ derive newtype instance arbitraryB ∷ Arbitrary B
 derive newtype instance boundedB ∷ Bounded B
 derive newtype instance boundedEnumB :: BoundedEnum B
 derive newtype instance coarbitraryB ∷ Coarbitrary B
+derive newtype instance enumB :: Enum B
 derive newtype instance eqB ∷ Eq B
 derive newtype instance ordB ∷ Ord B
 derive newtype instance semigroupB ∷ Semigroup B
@@ -43,6 +46,7 @@ derive newtype instance arbitraryC ∷ Arbitrary C
 derive newtype instance boundedC ∷ Bounded C
 derive newtype instance boundedEnumC :: BoundedEnum C
 derive newtype instance coarbitraryC ∷ Coarbitrary C
+derive newtype instance enumC ∷ Enum C
 derive newtype instance eqC ∷ Eq C
 derive newtype instance ordC ∷ Ord C
 derive newtype instance semigroupC ∷ Semigroup C
@@ -54,6 +58,7 @@ derive newtype instance arbitraryD ∷ Arbitrary D
 derive newtype instance boundedD ∷ Bounded D
 derive newtype instance boundedEnumD :: BoundedEnum D
 derive newtype instance coarbitraryD ∷ Coarbitrary D
+derive newtype instance enumD ∷ Enum D
 derive newtype instance eqD ∷ Eq D
 derive newtype instance ordD ∷ Ord D
 derive newtype instance semigroupD ∷ Semigroup D
@@ -65,6 +70,7 @@ derive newtype instance arbitraryE ∷ Arbitrary E
 derive newtype instance boundedE ∷ Bounded E
 derive newtype instance boundedEnumE :: BoundedEnum E
 derive newtype instance coarbitraryE ∷ Coarbitrary E
+derive newtype instance enumE ∷ Enum E
 derive newtype instance eqE ∷ Eq E
 derive newtype instance ordE ∷ Ord E
 derive newtype instance semigroupE ∷ Semigroup E
