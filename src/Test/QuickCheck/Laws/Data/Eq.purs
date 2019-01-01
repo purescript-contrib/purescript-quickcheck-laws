91dcd84e96c8d83aaf905f17d870ccfae45b3aff
Athan Clark <athan.clark@gmail.com>
2019-01-01 12:45:39 -0700

    more reporting functions

--- src/Test/QuickCheck/Laws/Data/Eq.purs
+++ src/Test/QuickCheck/Laws/Data/Eq.purs
@@ -4,7 +4,8 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
-import Test.QuickCheck (quickCheck')
+import Test.QuickCheck (quickCheck', Result, (===), (/==))
+import Test.QuickCheck.Combinators ((==>), (&=&), not')
 import Test.QuickCheck.Arbitrary (class Arbitrary)
 import Type.Proxy (Proxy)
 
@@ -45,3 +46,44 @@ checkEq _ = do
 
   negation ∷ a → a → Boolean
   negation x y = (x /= y) == not (x == y)
+
+
+-- | Like `checkEq`, with better error reporting.
+-- | - Reflexivity: `x == x = true`
+-- | - Symmetry: `x == y = y == x`
+-- | - Transitivity: if `x == y` and `y == z` then `x == z`
+-- | - Negation: `x /= y = not (x == y)`
+checkEqShow
+  ∷ ∀ a
+  . Arbitrary a
+  ⇒ Eq a
+  ⇒ Show a
+  ⇒ Proxy a
+  → Effect Unit
+checkEqShow _ = do
+
+  log "Checking 'Reflexivity' law for Eq"
+  quickCheck' 1000 reflexivity
+
+  log "Checking 'Symmetry' law for Eq"
+  quickCheck' 1000 symmetry
+
+  log "Checking 'Transitivity' law for Eq"
+  quickCheck' 1000 transitivity
+
+  log "Checking 'Negation' law for Eq"
+  quickCheck' 1000 negation
+
+  where
+
+  reflexivity ∷ a → Result
+  reflexivity x = x === x
+
+  symmetry ∷ a → a → Result
+  symmetry x y = ((x === y) ==> (y === x)) &=& ((x /== y) ==> (y /== x))
+
+  transitivity ∷ a → a → a → Result
+  transitivity x y z = ((x === y) &=& (y === x)) ==> (x === z)
+
+  negation ∷ a → a → Result
+  negation x y = (x /== y) ==> not' "Shouldn't be equal" (x === y)
