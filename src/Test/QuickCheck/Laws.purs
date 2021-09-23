b2ae0e6e6567efd641419f2cfaa783ee09133919
JordanMartinez <jordanalex.martinez@protonmail.com>
2021-09-23 06:27:10 -0700

    Remove duplicate Enum instances

--- src/Test/QuickCheck/Laws.purs
+++ src/Test/QuickCheck/Laws.purs
@@ -20,7 +20,6 @@ derive newtype instance boundedA ∷ Bounded A
 derive newtype instance enumA :: Enum A
 derive newtype instance boundedEnumA :: BoundedEnum A
 derive newtype instance coarbitraryA ∷ Coarbitrary A
-derive newtype instance enumA ∷ Enum A
 derive newtype instance eqA ∷ Eq A
 derive newtype instance ordA ∷ Ord A
 derive newtype instance semigroupA ∷ Semigroup A
@@ -33,7 +32,6 @@ derive newtype instance boundedB ∷ Bounded B
 derive newtype instance enumB :: Enum B
 derive newtype instance boundedEnumB :: BoundedEnum B
 derive newtype instance coarbitraryB ∷ Coarbitrary B
-derive newtype instance enumB ∷ Enum B
 derive newtype instance eqB ∷ Eq B
 derive newtype instance ordB ∷ Ord B
 derive newtype instance semigroupB ∷ Semigroup B
@@ -46,7 +44,6 @@ derive newtype instance boundedC ∷ Bounded C
 derive newtype instance enumC :: Enum C
 derive newtype instance boundedEnumC :: BoundedEnum C
 derive newtype instance coarbitraryC ∷ Coarbitrary C
-derive newtype instance enumC ∷ Enum C
 derive newtype instance eqC ∷ Eq C
 derive newtype instance ordC ∷ Ord C
 derive newtype instance semigroupC ∷ Semigroup C
@@ -59,7 +56,6 @@ derive newtype instance boundedD ∷ Bounded D
 derive newtype instance enumD :: Enum D
 derive newtype instance boundedEnumD :: BoundedEnum D
 derive newtype instance coarbitraryD ∷ Coarbitrary D
-derive newtype instance enumD ∷ Enum D
 derive newtype instance eqD ∷ Eq D
 derive newtype instance ordD ∷ Ord D
 derive newtype instance semigroupD ∷ Semigroup D
@@ -72,7 +68,6 @@ derive newtype instance boundedE ∷ Bounded E
 derive newtype instance enumE :: Enum E
 derive newtype instance boundedEnumE :: BoundedEnum E
 derive newtype instance coarbitraryE ∷ Coarbitrary E
-derive newtype instance enumE ∷ Enum E
 derive newtype instance eqE ∷ Eq E
 derive newtype instance ordE ∷ Ord E
 derive newtype instance semigroupE ∷ Semigroup E
