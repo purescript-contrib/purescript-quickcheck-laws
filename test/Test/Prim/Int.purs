78e5561baf70dd96c1a66275ffc4f2e9b9586423
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-09-27 07:52:19 -0700

    Refactor to use checkEuclideanRingGen

--- test/Test/Prim/Int.purs
+++ test/Test/Prim/Int.purs
@@ -3,23 +3,10 @@ module Test.Prim.Int where
 import Prelude
 
 import Effect (Effect)
-import Test.QuickCheck (class Arbitrary)
 import Test.QuickCheck.Gen (chooseInt)
 import Test.QuickCheck.Laws (checkLaws)
 import Test.QuickCheck.Laws.Data as Data
 import Type.Proxy (Proxy(..))
-
--- | Necessary for the EuclideanRing test
--- | so as to prevent integer overflow when multiplying large integer values
-newtype EuclideanRingInt = EuclideanRingInt Int
-derive newtype instance eqEuclideanRingInt :: Eq EuclideanRingInt
-derive newtype instance semiringEuclideanRingInt :: Semiring EuclideanRingInt
-derive newtype instance ringEuclideanRingInt :: Ring EuclideanRingInt
-derive newtype instance commutativeRingEuclideanRingInt :: CommutativeRing EuclideanRingInt
-derive newtype instance euclideanRingEuclideanRingInt :: EuclideanRing EuclideanRingInt
-instance arbitraryEuclideanRingInt :: Arbitrary EuclideanRingInt where
-  arbitrary = do
-    EuclideanRingInt <$> chooseInt (-10_000) 10_000
 
 checkInt ∷ Effect Unit
 checkInt = checkLaws "Int" do
@@ -27,8 +14,9 @@ checkInt = checkLaws "Int" do
   Data.checkOrd prxInt
   Data.checkCommutativeRing prxInt
   Data.checkSemiring prxInt
-  Data.checkEuclideanRing prxEuclideanRingInt
+  -- Necessary for the EuclideanRing test
+  -- so as to prevent integer overflow when multiplying large integer values
+  Data.checkEuclideanRingGen (chooseInt (-10_000) 10_000)
   Data.checkRing prxInt
   where
   prxInt = Proxy ∷ Proxy Int
-  prxEuclideanRingInt = Proxy ∷ Proxy EuclideanRingInt
